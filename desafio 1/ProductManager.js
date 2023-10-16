d11e6b7fad0c1947cbabb4c14c5ad19c0ab846a3
Carlos Scala <carloss@awlatam.com>
2023-10-15 21:50:57 -0300

    correcciones

--- desafio 1/ProductManager.js
+++ desafio 1/ProductManager.js
@@ -1,6 +1,17 @@
+class Product {
+    constructor(title, description, price, thumbnail, code, stock){
+        this.id = null,
+        this.title = title
+        this.description = description
+        this.price = price
+        this.thumbnail = thumbnail
+        this.code = code
+        this.stock = stock
+    }
+}
 class ProductManager {
     constructor() {
-        this.productos = [];
+        this.productos = []
     }
     
     
@@ -8,36 +19,23 @@ class ProductManager {
         return this.productos
     }
     
-    
     addProduct(title, description, price, thumbnail, code, stock) {
-        try {
-            this.findProduct(code)
+        if (!title || !description || !price || !thumbnail || !code || !stock){
+            console.log('Todos los campos son obligatorios')
+            return
+        }
+        const find = this.productos.find((item) => item.code === code)
+        if (!find){
             const id = this.productos.length + 1
-            const producto = {
-                id: id,
-                title: title,
-                description: description,
-                price: price,
-                thumbnail: thumbnail,
-                code: code,
-                stock: stock,
-            };
-
+            const producto = new Product(title, description, price, thumbnail, code, stock)
+            producto.id = id
             this.productos.push(producto)
             return producto
-        } catch (error) {
-            throw error;
-        }    
-        
-    }
-
-    findProduct(code){
-        const find = this.productos.find((item) => item.code === code);
-        if (find) {
-            throw new Error(`Producto existente: ${JSON.stringify(find)}`)
-        }else{
-            false
-        };
+        }
+        else{
+            console.log(`Producto existe id: ${find.id}`)
+            return
+        }
     }
 
     getProductById(id){
@@ -45,12 +43,14 @@ class ProductManager {
         if (find) {
             return find
         }else{
-            throw new Error(`Producto no existe id: ${id}`)
+            console.log(`Producto no existe id: ${id}`)
+            return
         };
     }
     
 }
-    
+
+///PROCESO DE TESTING
 const productManagerInstance = new ProductManager()
 let producto
 let productoPorId
@@ -59,48 +59,44 @@ let productoNoExiste
 
 console.log("1 - Agregar Producto:")
 // Agrega un producto
-try {
-    producto = productManagerInstance.addProduct("Nuevo producto", "Producto descripcion generica", 100.00, "noImage", "001", 999)
-    console.log(`Producto Agregado ${JSON.stringify(producto)}`)
+producto = productManagerInstance.addProduct("Producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25)
+if (producto){
+    console.log('Producto Agregado')
+    console.log(producto)
 }
-catch (error){
-    console.log(error)
-}
-console.log("-------------------------------\n")
 
 // Obtiene todos los Productos
 console.log("2 - Obtener Productos:")
 console.log(productManagerInstance.getProducts())
-console.log("-------------------------------\n")
 
 // Obtiene el producto por id
 console.log("3 - Obtener Producto por Id:")
-try {
-    productoPorId = productManagerInstance.getProductById(producto.id)
-    console.log(`Producto por Id obtenido ${JSON.stringify(productoPorId)}`)
+productoPorId = productManagerInstance.getProductById(producto.id)
+if (productoPorId){
+    console.log('Producto por Id obtenido')
+    console.log(productoPorId)
 }
-catch (error){
-    console.log(error)
-}
-console.log("-------------------------------\n")
 
 // Inserta producto repetido
 console.log("4 - Agregar Producto existente:")
-try {
-    productoRepetido = productManagerInstance.addProduct("Nuevo producto", "Producto descripcion generica", 100.00, "noImage", "001", 999)
-    console.log(`Producto Agregado ${JSON.stringify(productoRepetido)}`)
-    
-} catch (error) {
-    console.log(error)
+productoRepetido = productManagerInstance.addProduct("Producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25)
+if (productoRepetido){
+    console.log('Producto Agregado')
+    console.log(productoRepetido)
 }
-console.log("-------------------------------\n")
 
 // Obtiene el producto por id y este no existe
 console.log("5 - Obtener Producto por Id que no existe:")
-try {
-    productoNoExiste = productManagerInstance.getProductById(1000)
-    console.log(`Producto por Id obtenido ${JSON.stringify(productoNoExiste)}`)
-} catch (error) {
-    console.log(error)
+productoNoExiste = productManagerInstance.getProductById(1000)
+if (productoNoExiste){
+    console.log('Producto por Id obtenido')
+    console.log(productoNoExiste)
+} 
+
+console.log("6 - Agregar Producto sin todos los campos:")
+// Agrega un producto
+producto = productManagerInstance.addProduct("Producto prueba", "Este es un producto prueba", null, "Sin imagen", "abc123", 25)
+if (producto){
+    console.log('Producto Agregado')
+    console.log(producto)
 }
-console.log("-------------------------------\n")
