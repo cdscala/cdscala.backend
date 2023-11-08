5c0893f657626fc54c1c91f2b34c8e1bbeff6174
carlos scala <carlosd.scala@gmail.com>
2023-11-08 10:57:29 -0300

    socket expansion 2

--- desafio 4/src/app.js
+++ desafio 4/src/app.js
@@ -36,8 +36,10 @@ let prods = await prodsManager.getProducts()
 io.on('connection', (socket) => {
     console.log('Un cliente se ha conectado');
     socket.emit('lista',prods)
-    socket.on('add-product')
-
+    socket.on('delete-product',async (value) =>{
+        await prodsManager.deleteProduct(value)
+        prods = await prodsManager.getProducts()
+    })
 });
 httpServer.listen(port, hostname,  () => { console.log(`Server corriendo en http://${hostname}:${port}/`) })
 
--- desafio 4/src/public/js/index.js
+++ desafio 4/src/public/js/index.js
@@ -1,7 +1,7 @@
 const socket = io();
 
 socket.on("lista",(data) => {
-    console.log(data)
+    // console.log(data)
     renderProductItem(data)
 })
 
@@ -18,24 +18,42 @@ function renderProductItem(data){
             <td>${product.stock}</td>
             <td>${product.status}</td>
             <td>${product.category}</td>
+            <td><button value=${product.id} onclick=${deleteProductoTrigger(product.id)} class="cs-delete-btn">Delete</button></td>
         </tr>
         `
     }).join(" ")
     document.getElementById('product-list').innerHTML = html
 }
+function deleteProduct(id){
+    console.log(`eliminar producto: ${id}`)
+    socket.emit('delete-product',id)
+    
+    Toastify({
+        text: `El producto eliminado fue: ${id} a traves de websocket`,
+        duration: 3000,
+        destination: "",
+        newWindow: true,
+        close: true,
+        gravity: "top", 
+        position: "center", 
+        stopOnFocus: true, 
+        style: {
+        },
+        onClick: function(){} 
+    }).showToast();
+}
+function deleteProductoTrigger(id){
+    return `deleteProduct(${id})`
+}
 
-const form = document.getElementById('form');
-const input = document.getElementById('input');
+// const form = document.getElementById('form');
+// const input = document.getElementById('input');
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    if (input.value) {
-        socket.emit('add-product', input.value);
-        input.value = '';
-    }
-    Swal.fire({
-        title: "Good job!",
-        text: "You clicked the button!",
-        icon: "success"
-      });
-});
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     if (input.value) {
+//         socket.emit('add-product', input.value);
+//         input.value = '';
+//     }
+
+// });
