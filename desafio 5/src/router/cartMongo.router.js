5507afffd569ec8288fbfa13120fd3ad8969863f
carlos scala <carlosd.scala@gmail.com>
2023-11-23 22:40:15 -0300

    commit actualizacion

--- desafio 5/src/router/cartMongo.router.js
+++ desafio 5/src/router/cartMongo.router.js
@@ -4,11 +4,10 @@ import CartModel from '../models/cart.model';
 
 // Obtener lista de productos del carrito por ID (GET)
 cartMongoRouter.get('/:id', async (req, res) => {
-    const id = parseInt(req.params.id);
     try {
-        const cart = await CartModel.findById(id)
+        const cart = await CartModel.findById(req.params.id)
         if (!cart) {
-            return res.status(404).json({ message: 'Producto no encontrado' });
+            return res.status(404).json({ message: 'Carrp no encontrado' });
         }
         res.json(cart);
     } catch (error) {
@@ -18,16 +17,38 @@ cartMongoRouter.get('/:id', async (req, res) => {
 
 // Crear un carrito (POST)
 cartMongoRouter.post('/', async (req, res) => {
-    
+    const cart = new CartModel(req.body);
+    try {
+      const nuevoCart = await cart.save();
+      res.status(201).json(nuevoCart);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
 });
 
 // Agregar un nuevo producto en el carrito (POST)
 cartMongoRouter.post('/:cid/product/:pid', async (req, res) => {
-    
+    try {
+        const cart = await CartModel.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!cart) {
+          return res.status(404).json({ message: 'Producto no encontrado' });
+        }
+        res.json(cart);
+      } catch (error) {
+        res.status(500).json({ message: error.message });
+      }
 });
 // Eliminar un carrito (DELETE)
 cartMongoRouter.delete('/:id', async (req, res) => {
-    
+    try {
+        const cart = await CartModel.findByIdAndDelete(req.params.id);
+        if (!cart) {
+          return res.status(404).json({ message: 'Carro no encontrado' });
+        }
+        res.json({ message: 'Carro eliminado exitosamente' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 });
 
 export {cartMongoRouter as cartRouter}
